3faa86e6d1225387f4529a68468b2d2c069f9747
kirthiga127 <81847005+kirthiga127@users.noreply.github.com>
2022-05-28 21:37:34 +0530

    Updated

--- src/main/java/com/pms/portal/controller/PensionerDetailController.java
+++ src/main/java/com/pms/portal/controller/PensionerDetailController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -215,6 +217,9 @@ public class PensionerDetailController {
 		
 		return model;
 	}
-	
+	@GetMapping("/health-check")
+	public ResponseEntity<String> healthCheck() {
+		return new ResponseEntity<>("auth-Ok", HttpStatus.OK);
+	}
 	
 }
